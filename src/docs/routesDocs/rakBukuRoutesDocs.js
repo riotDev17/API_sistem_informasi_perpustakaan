7c9b650aaa0c96f02561723007bd68d1877f1cf3
riotDev17 <tomasalberto527@gmail.com>
2023-12-15 13:21:12 +0700

    docs: Add Documentation for UPDATE RAK BUKU API

--- src/docs/routesDocs/rakBukuRoutesDocs.js
+++ src/docs/routesDocs/rakBukuRoutesDocs.js
@@ -101,7 +101,7 @@
  *
  * */
 
-// ======= GET RAK BUKU BY ID=======
+// ======= GET RAK BUKU BY ID =======
 /**
  * @openapi
  * /api/rak-buku/{rakBukuId}:
@@ -135,5 +135,61 @@
  *          application/json:
  *            schema:
  *              $ref: '#/components/schemas/Get Rak Buku By ID Not Found'
+ *
+ * */
+
+
+// ======= UPDATE RAK BUKU =======
+/**
+ * @openapi
+ * /api/rak-buku/{rakBukuId}:
+ *  put:
+ *    tags:
+ *      - Rak Buku
+ *    summary: Update Rak Buku
+ *    parameters:
+ *      - name: rakBukuId
+ *        in: path
+ *        required: true
+ *        description: Rak Buku ID
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Update Rak Buku'
+ *    responses:
+ *      200:
+ *        description: Update Rak Buku Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Rak Buku Success'
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Rak Buku Bad Request'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Rak Buku Unauthorized'
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Rak Buku Not Found'
+ *      409:
+ *        description: Conflict
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Rak Buku Conflict'
  *
  * */
--- src/docs/serviceDocs/rakBukuServiceDocs.js
+++ src/docs/serviceDocs/rakBukuServiceDocs.js
@@ -241,3 +241,89 @@
  *          description: message
  *
  * */
+
+// ======= UPDATE RAK BUKU =======
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    Update Rak Buku:
+ *      type: object
+ *      required:
+ *        - nama_rak_buku
+ *      properties:
+ *        nama_rak_buku:
+ *          type: string
+ *          description: nama rak buku
+ *    Update Rak Buku Success:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          default: Success
+ *          description: Success
+ *        message:
+ *          type: string
+ *          default: Message Success
+ *          description: message
+ *        data:
+ *          type: object
+ *          properties:
+ *            id_rak_buku:
+ *              type: string
+ *              description: id rak buku
+ *            nama_rak_buku:
+ *              type: string
+ *              description: nama rak buku
+ *            createdAt:
+ *              type: string
+ *              description: waktu dibuat
+ *            updatedAt:
+ *              type: string
+ *              description: waktu diupdate
+ *    Update Rak Buku Bad Request:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          default: Bad Request
+ *          description: Bad Request
+ *        message:
+ *          type: string
+ *          default: Message Bad Request
+ *          description: message
+ *    Update Rak Buku Unauthorized:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          default: Unauthorized
+ *          description: Unauthorized
+ *        message:
+ *          type: string
+ *          default: Message Unauthorized
+ *          description: message
+ *    Update Rak Buku Not Found:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          default: Not Found
+ *          description: Not Found
+ *        message:
+ *          type: string
+ *          default: Message Not Found
+ *          description: message
+ *    Update Rak Buku Conflict:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          default: Conflict
+ *          description: Conflict
+ *        message:
+ *          type: string
+ *          default: Message Conflict
+ *          description: message
+ *
+ * */
