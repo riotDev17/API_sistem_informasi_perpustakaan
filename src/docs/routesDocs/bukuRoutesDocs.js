969e6e98f63904e91f7fffde2f5e7795bf967920
riotDev17 <tomasalberto527@gmail.com>
2023-12-15 20:51:09 +0700

    docs: Add Documentation For CREATE BUKU API

---
+++ src/docs/routesDocs/bukuRoutesDocs.js
@@ -0,0 +1,41 @@
+// ====== ADD BUKU ======
+/**
+ * @openapi
+ * /api/buku:
+ *  post:
+ *    tags:
+ *      - Buku
+ *    summary: Add Buku
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            $ref: '#/components/schemas/Add Buku'
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Buku Success'
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Buku Bad Request'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Buku Unauthorized'
+ *      409:
+ *        description: Conflict
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Buku Conflict'
+ *
+ * */
---
+++ src/docs/serviceDocs/bukuServiceDocs.js
@@ -0,0 +1,124 @@
+// ====== ADD BUKU ======
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    Add Buku:
+ *      type: object
+ *      required:
+ *        - judul_buku
+ *        - pengarang
+ *        - penerbit
+ *        - tahun_terbit
+ *        - deskripsi
+ *        - stok_buku
+ *        - id_rak_buku
+ *      properties:
+ *        judul_buku:
+ *          type: string
+ *          description: judul buku
+ *        pengarang:
+ *          type: string
+ *          description: pengarang buku
+ *        penerbit:
+ *          type: string
+ *          description: penerbit buku
+ *        tahun_terbit:
+ *          type: string
+ *          description: tahun terbit buku
+ *        deskripsi:
+ *          type: string
+ *          description: deskripsi buku
+ *        stok_buku:
+ *          type: string
+ *          description: stok buku
+ *        foto_buku:
+ *          type: file
+ *          description: foto buku
+ *        id_rak_buku:
+ *          type: string
+ *          description: id rak buku
+ *    Add Buku Success:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          description: status response
+ *        message:
+ *          type: string
+ *          description: message response
+ *        data:
+ *          type: object
+ *          properties:
+ *            id_buku:
+ *              type: string
+ *              description: id buku
+ *            judul_buku:
+ *              type: string
+ *              description: judul buku
+ *            pengarang:
+ *              type: string
+ *              description: pengarang buku
+ *            penerbit:
+ *              type: string
+ *              description: penerbit buku
+ *            tahun_terbit:
+ *              type: string
+ *              description: tahun terbit buku
+ *            deskripsi:
+ *              type: string
+ *              description: deskripsi buku
+ *            stok_buku:
+ *              type: string
+ *              description: stok buku
+ *            foto_buku:
+ *              type: string
+ *              description: foto buku
+ *            rak_buku:
+ *              type: object
+ *              properties:
+ *                id_rak_buku:
+ *                  type: string
+ *                  description: id rak buku
+ *                nama_rak_buku:
+ *                  type: string
+ *                  description: nama rak buku
+ *            createdAt:
+ *              type: string
+ *              description: create date
+ *            updatedAt:
+ *              type: string
+ *              description: update date
+ *    Add Buku Bad Request:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          description: status response
+ *        message:
+ *          type: string
+ *          default: Message Bad Request
+ *          description: message response
+ *    Add Buku Unauthorized:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          description: status response
+ *        message:
+ *          type: string
+ *          default: Message Unauthorized
+ *          description: message response
+ *    Add Buku Conflict:
+ *      type: object
+ *      properties:
+ *        status:
+ *          type: string
+ *          description: status response
+ *        message:
+ *          type: string
+ *          default: Message Conflict
+ *          description: message response
+ *
+ *
+ * */
