0e45654ea7cdfefdb548f9db7076b4c0a8c9cd38
riotDev17 <tomasalberto527@gmail.com>
2023-12-16 12:26:56 +0700

    helper: Add random number function

---
+++ src/helpers/randomNumber.js
@@ -0,0 +1,11 @@
+const randomNumber = () => {
+  let number = '';
+  for (let i = 0; i < 10; i++) {
+    number += Math.floor(Math.random() * 10);
+  }
+
+  return number;
+};
+
+
+export { randomNumber };
