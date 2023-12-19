2b1419c4d99556d95f87463e67c8046977ec3a3c
riotDev17 <tomasalberto527@gmail.com>
2023-12-19 19:25:20 +0700

    helper: Add Helper random number

--- src/helpers/randomNumber.js
+++ src/helpers/randomNumber.js
@@ -1,10 +1,12 @@
 const randomNumber = () => {
+  const max32BitInt = Math.pow(2, 31) - 1; // Maximum value for a 32-bit signed integer
   let number = '';
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 9; i++) { // Reduced to 9 digits to fit within range
     number += Math.floor(Math.random() * 10);
   }
 
-  return parseInt(number);
+  return parseInt(number) % max32BitInt; // Modulo to ensure the number fits within the 32-bit range
 };
+
 
 export { randomNumber };
