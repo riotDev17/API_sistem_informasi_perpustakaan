0c66a1058219d89c33475dafab79159971e77334
riotDev17 <tomasalberto527@gmail.com>
2023-12-19 20:50:48 +0700

    docs: Add documentation for search riwayat API

--- src/docs/routesDocs/riwayatRoutesDocs.js
+++ src/docs/routesDocs/riwayatRoutesDocs.js
@@ -22,3 +22,47 @@
  *              $ref: '#/components/schemas/Get Riwayat Unauthorized'
  *
  * */
+
+// ======= SEARCH RIWAYAT =======
+/**
+ * @openapi
+ * /api/riwayat/search:
+ *  get:
+ *    tags:
+ *      - Riwayat
+ *    summary: Search Riwayat
+ *    description: Search Riwayat
+ *    parameters:
+ *      - name: tanggal_pinjam
+ *        in: query
+ *        required: true
+ *        description: search tanggal pinjam
+ *        schema:
+ *          type: string
+ *      - name: tanggal_kembali
+ *        in: query
+ *        required: false
+ *        description: search tanggal kembali
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Search Riwayat Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Search Riwayat Success'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Search Riwayat Unauthorized'
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Search Riwayat Not Found'
+ *
+ * */
